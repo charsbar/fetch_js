dee1e218c12fa740a82239a1491cb7c1b5226cf2
Kenichi Ishigaki <ishigaki@cpan.org>
2012-02-27 15:51:01 +0900

    html5.js

---
+++ lib/FetchJS/Html5.pm
@@ -0,0 +1,42 @@
+package FetchJS::Html5;
+
+use strict;
+use warnings;
+use base 'FetchJS';
+
+sub _run {
+  my ($self, $version) = @_;
+
+  $version ||= 'master';
+  my $uri = "https://github.com/aFarkas/html5shiv/zipball/$version";
+
+  my $zipball = $self->fetch_furl($uri => "html5.zip") or return;
+  $self->unzip($zipball, (
+    [qr{src/.+\.js$} => sub { $_[0] =~ s/\bsrc\b/js/ }],
+  ));
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+FetchJS::Html5 - fetch html5.js
+
+=head1 SYNOPSIS
+
+  fetch_js html5
+
+=head1 AUTHOR
+
+Kenichi Ishigaki, E<lt>ishigaki@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Kenichi Ishigaki.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
